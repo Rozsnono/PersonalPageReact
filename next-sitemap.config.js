d0a22de2b8f6e76be81ecd6a8193e4727d83194e
rozsnorbert <rozsnono@gmail.com>
2025-06-28 00:23:40 +0200

    sitemap

--- next-sitemap.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-    siteUrl: 'https://rozsnorbert.hu',
-    sitemapSize: 7000,
-    changefreq: 'never',
-    priority: 0.7,
-    trailingSlash: false,
-    generateRobotsTxt: true,
-};
